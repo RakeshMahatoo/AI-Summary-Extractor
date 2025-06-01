b29a5398712317ab730e9efc9911f55304745109
RAKESH MAHATO <RakeshMahatoo@example.com>
2025-06-01 22:02:35 +0530

    functional

--- content.js
+++ content.js
@@ -1,16 +1,15 @@
-function getArticleText(){
-    const article = document.querySelector("article");
-    if(article) return article.innerText;
+function getArticleText() {
+  const article = document.querySelector("article");
+  if (article) return article.innerText;
 
-    const paragraphs = Array.from(document.querySelectorAll("p"));
-    return paragraphs.map((p)=> p.innerText).join("\n");
-
+  // fallback
+  const paragraphs = Array.from(document.querySelectorAll("p"));
+  return paragraphs.map((p) => p.innerText).join("\n");
 }
 
-chrome.runtime.onMessage.addListner((req,_sender, sendResponse)=>{
-if((req.type='GET_ARTICLE_TEXT')){
-    const text = getArticleText()
-    sendResponse({text});
-}
-
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+  if (req.type === "GET_ARTICLE_TEXT") {
+    const text = getArticleText();
+    sendResponse({ text });
+  }
 });
